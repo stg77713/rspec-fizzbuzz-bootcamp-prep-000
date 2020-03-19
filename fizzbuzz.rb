aec542c2c4082468bbf461c116da1a17d6c6297d
George Lugo <stg77713@gmail.com>
2020-03-19 01:21:43 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,6 +4,12 @@ def fizzbuzz(num)
   if num % 3 == 0
     return "Fizz"
   end
+  if num % 5 == 0 
+    return "Buzz"
+  end 
+  if num % 3 == 0 && num % 5 == 0 
+    return "FizzBuzz"
+  end 
 end 
 
 
