f349b598d9c4bae9f1af47974626646bf43be7f6
George Lugo <stg77713@gmail.com>
2020-03-19 01:25:18 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
+  # as you see in this code did not work when num % 3 
+  # && num % 5 == 0 was at the bottom of the code 
   if num % 3 == 0 && num % 5 == 0 
     return "FizzBuzz"
   end 
