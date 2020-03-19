3c71f1a9346cf79b4725cee203ff38c847656f26
George Lugo <stg77713@gmail.com>
2020-03-19 01:13:39 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
-  
+def fizzbuzz(num)
+  if (num % 3 === 0) {
+    puts "Fizz"
+  }elsif (num % 5 === 0){
+    puts "Buzz"
+  }elsif (num)
 end 
